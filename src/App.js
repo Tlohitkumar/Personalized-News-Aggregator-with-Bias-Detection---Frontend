49861f30facb2df3ec4436341217a825cf3a5153
Tlohitkumar <lohitkumarthotakura@gmail.com>
2026-03-31 17:35:42 +0530

    Updated an App.js to connect the Backed Code to Frontend code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getUsers } from "./services/userService";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    getUsers().then((res) => {
+      setUsers(res.data);
+    });
+  }, []);
+
   return (
     <div>
-      <h1>News App 🚀</h1>
-      <p>Day 1 Setup Complete</p>
+      <h1>Users List</h1>
+      {users.map((u) => (
+        <p key={u.id}>{u.name} - {u.email}</p>
+      ))}
     </div>
   );
 }
