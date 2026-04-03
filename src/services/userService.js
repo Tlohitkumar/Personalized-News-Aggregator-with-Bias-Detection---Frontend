0db06a3befd77afa988afeb4d83ece6c26b84c2f
Tlohitkumar <lohitkumarthotakura@gmail.com>
2026-04-03 16:20:16 +0530

    enchaced the ui useibility

--- src/services/userService.js
+++ src/services/userService.js
@@ -2,10 +2,22 @@ import axios from "axios";
 
 const API_URL = "http://localhost:8080/api/users";
 
-export const getUsers = () => axios.get(API_URL);
+const getAuthHeader = () => {
+  return {
+    headers: {
+      Authorization: "Bearer " + localStorage.getItem("token"),
+    },
+  };
+};
 
-export const addUser = (user) => axios.post(`${API_URL}/register`, user);
+export const getUsers = () =>
+  axios.get(API_URL, getAuthHeader());
 
-export const deleteUser = (id) => axios.delete(`${API_URL}/${id}`);
+export const addUser = (user) =>
+  axios.post(`${API_URL}/register`, user);
 
-export const loginUser = (user) => axios.post(`${API_URL}/login`, user);
+export const deleteUser = (id) =>
+  axios.delete(`${API_URL}/${id}`, getAuthHeader());
+
+export const loginUser = (user) =>
+  axios.post(`${API_URL}/login`, user);
