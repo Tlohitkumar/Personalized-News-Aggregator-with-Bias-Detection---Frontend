af5d8e748a26ac08bf1431f4c930aec8bc47b1f0
Tlohitkumar <lohitkumarthotakura@gmail.com>
2026-04-15 16:10:35 +0530

    Updated the Layout code for neat structure

--- src/App.js
+++ src/App.js
@@ -4,16 +4,38 @@ import "./App.css";
 
 function App() {
   const [news, setNews] = useState([]);
+  const [keyword, setKeyword] = useState("");
 
   useEffect(() => {
-    getNews()
+    loadNews();
+  }, []);
+
+  const loadNews = () => {
+    getNews(keyword)
       .then(res => setNews(res.data.articles))
       .catch(err => console.log(err));
-  }, []);
+  };
 
   return (
     <div className="container">
       <h1>📰 Top News</h1>
+
+      {/* 🔍 Search */}
+      <input
+        type="text"
+        placeholder="Search news..."
+        value={keyword}
+        onChange={(e) => setKeyword(e.target.value)}
+      />
+
+      <button onClick={loadNews}>Search</button>
+
+      {/* 📂 Categories */}
+      <div>
+        <button onClick={() => getNews(null, "sports").then(res => setNews(res.data.articles))}>Sports</button>
+        <button onClick={() => getNews(null, "technology").then(res => setNews(res.data.articles))}>Tech</button>
+        <button onClick={() => getNews(null, "business").then(res => setNews(res.data.articles))}>Business</button>
+      </div>
 
       <div className="news-grid">
         {news.map((item, index) => (
